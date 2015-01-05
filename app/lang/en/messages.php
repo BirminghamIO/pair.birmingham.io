96cd268620240e7644df18093bd5b9d60984ba16
pitkostrzewa <piotrek.kostrzewa@outlook.com>
2015-01-05 18:32:16 +0100

    messages

--- app/lang/en/messages.php
+++ app/lang/en/messages.php
@@ -5,10 +5,11 @@ return array(
 	'failed_login' => 'Uncorrect login or password.',
     'password_changed' => 'Your password has been changed',
     'different_passwords' => 'Given passwords are not the same',
-    'editing_completed' => 'Your has already changed',
+    'editing_completed' => 'Your profile has been updated',
     
     'github_connected' => 'Your account is conected with GitHub now',
     'github_not_connected' => 'Sorry, we have got a problem. Your account is stil not connected with GitHub',
     'github_disconnected' => 'Your account is disconected from GitHub now',
     'github_not_disconnected' => 'Sorry, we have got a problem. Your account is still connected with GitHub'
+    
 );
